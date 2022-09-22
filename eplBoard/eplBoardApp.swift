a67cdd02115b9c8a16531e6ab61a4a8d645e071f
habaek <haback1493@gmail.com>
2022-09-22 14:01:39 +0900

    Initial Commit

---
+++ eplBoard/eplBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  eplBoardApp.swift
+//  eplBoard
+//
+//  Created by macbook1 on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct eplBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
