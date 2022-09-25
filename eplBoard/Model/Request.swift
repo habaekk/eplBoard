8d1ea0fcd67e66fdb58cb738385ebdedce5cd00a
habaek <haback1493@gmail.com>
2022-09-25 23:12:21 +0900

    .

--- eplBoard/Model/Request.swift
+++ eplBoard/Model/Request.swift
@@ -8,22 +8,4 @@
 import Foundation
 
 struct Request {
-    func performRequest(with urlString: String) {
-        if let url = URL(string: urlString) {
-            let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { (data, response, error) in
-                if error != nil {
-                    print(error!)
-                    return
-                }
-                
-                if let safeData = data {
-                    if let teams = Parsing.parseJSON(safeData) {
-                        return
-                    }
-                }
-            }
-            task.resume()
-        }
-    }
 }
--- eplBoard/ViewModel/ViewModel.swift
+++ eplBoard/ViewModel/ViewModel.swift
@@ -7,6 +7,49 @@
 
 import Foundation
 
-struct ViewModel {
+protocol FootballDelegate {
+    func didUpdateFootball(_ viewmodel: ViewModel, information: Information)
+    func didFailWithError(error: Error)
+
+}
+
+class ViewModel {
+    
+    var delegate: FootballDelegate?
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let teams = self.parseJSON(safeData) {
+                        let id = String(teams.data[0].id)
+                        let title = teams.data[0].name
+                        let info = Information(id: id, title: title)
+                        self.delegate?.didUpdateFootball(self, information: info)
+                        
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ teamData: Data) -> TeamDetails? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(TeamDetails.self, from: teamData)
+            return decodedData
+        } catch {
+            self.delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+
     
 }
